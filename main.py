4ed3f2015878a92055c5f797facba53a9b265b11
Jaesik Moon <jsik@bigvalue.co.kr>
2025-06-20 11:13:32 +0900

    convexhull 적용

--- main.py
+++ main.py
@@ -23,30 +23,39 @@ def root():
 def get_hull(time: int = Query(...)):
     with engine.connect() as conn:
         result = conn.execute(text("""
-                SELECT json_agg(
-                json_build_object(
-                  'type', 'Feature',
-                  'geometry', ST_AsGeoJSON(ST_Transform(m.live_station_geometry, 4326))::json,
-                  'properties', json_build_object()
-                    )
-                ) AS features
-                FROM 문재식.tb_metropolitan_morning_commute_od_202407 m
-                JOIN (
-                    SELECT ST_Buffer(
-                               ST_Transform(
-                                   ST_SetSRID(
-                                       ST_MakePoint(126.976296, 37.563641), 4326), 5179
-                               ),
-                               300) AS geom
-                ) b
-                  ON ST_Within(m.work_station_geometry, b.geom)
-                WHERE m.morning_commute_average_time < :time
-        """), {"time": time})
+                                   WITH filtered AS (SELECT *,
+                                                            ST_Distance(
+                                                                    ST_Transform(m.live_station_geometry, 4326),
+                                                                    ST_SetSRID(ST_MakePoint(126.976296, 37.563641), 4326)
+                                                            ) AS dist
+                                                     FROM 문재식.tb_metropolitan_morning_commute_od_202407 m
+                                                              JOIN (SELECT ST_Buffer(
+                                                                                   ST_Transform(
+                                                                                           ST_SetSRID(
+                                                                                                   ST_MakePoint(126.976296, 37.563641),
+                                                                                                   4326), 5179
+                                                                                   ),
+                                                                                   300) AS geom) b
+                                                                   ON ST_Within(m.work_station_geometry, b.geom)
+                                                     WHERE m.morning_commute_average_time < :time),
+                                        no_outlier AS (SELECT *
+                                                       FROM filtered
+                                                       ORDER BY dist ASC
+                                                       OFFSET 0 LIMIT (SELECT GREATEST(COUNT(*) - 10, 0) FROM filtered)
+                                            -- 전체 중에서 가장 먼 10개만 버리고 나머지
+                                        )
+                                   SELECT ST_AsGeoJSON(
+                                                  ST_Transform(
+                                                          ST_ConvexHull(ST_Collect(no_outlier.live_station_geometry)),
+                                                          4326
+                                                  )
+                                          ) AS geojson
+                                   FROM no_outlier;
+                                   """), {"time": time})
         row = result.first()
-        features = row[0] if row else []
-        return JSONResponse(content={
-            "type": "FeatureCollection",
-            "features": features
-        })
-
+        if not row or not row[0]:
+            return JSONResponse(content={
+                "type": "Polygon",
+                "coordinates": []
+            })
         return JSONResponse(content=json.loads(row[0]))
